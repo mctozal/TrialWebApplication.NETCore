bb3b5f1078e20a19145b6b604a99ec9967c038ef
Mücahit Tozal <mctozal@gmail.com>
2019-06-13 17:12:57 +0300

    My first work structured. trial.web  =>  .net core app trial.webapi => API trial.business =>  class library added.

---
+++ TrialApplication/trialBusiness/trialBusiness/IUserManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace trialBusiness
+{
+    public interface IUserManagement
+    {
+
+        List<UserModel> GetSystemUserList(bool isActive = false);
+
+    }
+}
---
+++ TrialApplication/trialBusiness/trialBusiness/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trialBusiness
+{
+    public class UserModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public bool isComplete { get; set; }
+    }
+}
